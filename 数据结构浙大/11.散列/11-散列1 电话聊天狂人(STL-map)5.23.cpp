b40f490602371d43261b59da977938333f93e244
Mhhhaster <60060245+Mhhhaster@users.noreply.github.com>
2020-05-23 19:01:15 +0800

    update5.23

---
+++ 数据结构浙大/11.散列/11-散列1 电话聊天狂人(STL-map)5.23.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<map>
+#include<string>
+#include<vector>
+using namespace std;
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	map<string, int>Mmap;
+	string name;
+	N *= 2;
+	while (N--)
+	{
+		cin >> name;
+		auto it = Mmap.find(name);
+		if (Mmap.end() == it)//这个元素没有出现过
+			Mmap.insert({ name,1 });
+		else
+			it->second++;//按键大小排序，改变key值不影响
+	}
+	int maxcallnum = 0, same = 1;//分别记录最大的通话次数和相同次数的人
+	auto M = Mmap.begin();//M指示通话次数最大的位置
+	for (auto it = Mmap.begin(); it != Mmap.end(); it++)
+	{
+		if (it->second > maxcallnum)//更新最大通话次数
+		{
+			maxcallnum = it->second;
+			M = it;
+			same = 1;
+		}
+		else if (it->second == maxcallnum)
+			same++;
+	}
+	cout << M->first << " " << M->second;
+	if (same > 1)
+		cout << " " << same;
+}
---
+++ 数据结构浙大/11.散列/11-散列3 QQ帐户的申请与登陆(map)5.23.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include<string>
+#include<map>
+using namespace std;
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	map<string, string>M;
+	char c;
+	string id, pw;
+	while (N--)
+	{
+		cin >> c;
+		cin >> id >> pw;
+		auto it = M.find(id);
+		if (c == 'L')
+		{
+			if (it == M.end())//没有找到账号
+				cout << "ERROR: Not Exist" << endl;
+			else
+			{
+				if (it->second == pw)//密码正确
+					cout << "Login: OK" << endl;
+				else//密码错误
+					cout << "ERROR: Wrong PW" << endl;
+			}
+		}
+		else if (c == 'N')
+		{
+			if (it == M.end())//没有找到账号，新建
+			{
+				cout << "New: OK" << endl;
+				M.insert({ id, pw });//这里一定要注意insert的格式  insert({A,B})//小括号括大括号
+			}
+			else//找到了账号
+				cout << "ERROR: Exist"<<endl;
+		}
+	}
+	return 0;
+}
